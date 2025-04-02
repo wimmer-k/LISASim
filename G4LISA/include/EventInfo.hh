d77e5f28fdac80c5f89471b39e1600d1786b3312
wiktorsen <134003626+wiktorsen@users.noreply.github.com>
2025-04-02 13:37:17 +0200

    Add files via upload

--- G4LISA/include/EventInfo.hh
+++ G4LISA/include/EventInfo.hh
@@ -1,1 +1,172 @@
-../../Analysis/inc/EventInfo.hh
+#ifndef EventInformation_h
+#define EventInformation_h 1
+
+#ifdef ISSIM
+#include "G4VUserEventInformation.hh"
+#endif
+
+#include <vector>
+#include <iostream>
+
+#include "TObject.h"
+#include "TVector3.h"
+using namespace std;
+
+class LISAEvent : public TObject {
+public:
+  LISAEvent(){
+    Clear();
+  }
+  void Clear(Option_t * /*option*/ ="") override {
+    fEdet.clear();
+    fID.clear();
+    fX.clear();
+    fY.clear();
+    
+  }
+  void AddHit(int i, double E, int x, int y){
+    fID.push_back(i); 
+    fEdet.push_back(E);
+    fX.push_back(x);
+    fY.push_back(y);
+  }
+  void SetEdet(vector<double> Edet){fEdet = Edet;}
+  void SetLayerID(vector<int> id){fID = id;}
+  void SetX(vector<int> x){fX=x;}
+  void SetY(vector<int> y){fY=y;}
+
+
+
+  vector<double> GetEnergyDetected(){return fEdet;}
+  double GetEnergyDetected(int i){return fEdet[i];}
+  vector<int> GetLayers(){return fID;}
+  int GetLayerID(int i){return fID[i];}
+  int GetPosX(int x){return fX[x];}
+  int GetPosY(int y){return fY[y];}
+  void Print(Option_t * ="") const override {
+    for(UShort_t i=0;i<fEdet.size();i++)
+    cout << "ID = " << fID[i] << ", Edet = " << fEdet[i] <<"fX =  "<<fX[i]<<"fY =  " <<fY[i]<<endl;
+  } 
+   
+protected:
+  vector<double> fEdet;
+  vector<int> fID;
+  vector<int> fX ;
+  vector<int> fY ;
+
+  ClassDefOverride(LISAEvent, 1);
+};
+
+
+class SimEvent : public TObject {
+public:
+  SimEvent(){
+    Clear();
+  }
+  void Clear(Option_t * /*option*/ =""){
+    fincdir.SetXYZ(-100,0,0);
+    fincpos.SetXYZ(-10,0,0);
+    fEbeam = 0;
+
+    foutdir.SetXYZ(-100,0,0);
+    foutpos.SetXYZ(-10,0,0);
+    
+    fbetas.clear();
+    freacpos.SetXYZ(-10,0,0);
+    //freacpos.clear();
+  }
+  // setters
+  void SetNDiamonds(){}
+  void SetEventID(int n){feventID = n;}
+
+  void SetIncomingDirection(TVector3 dir){fincdir = dir;}
+  void SetIncomingPosition(TVector3 pos){fincpos = pos;}
+  void SetBeamEnergy(double Ebeam){fEbeam = Ebeam;}
+
+  void SetOutGoingDirection(TVector3 dir){foutdir = dir;}
+  void SetOutGoingPosition(TVector3 pos){foutpos = pos;}
+  void SetOutGoingBeta(vector<double> betas){fbetas = betas;}
+  void SetOutGoingBeta(int n,double beta){fbetas[n] = beta;}
+
+  void SetReactionPosition(TVector3 pos){freacpos = pos;}
+
+  void SetDimX (int dx){DimX=dx;}
+  void SetDimY (int dy){DimY=dy;}
+  void SetNLayers (int dL){NLayers=dL; fbetas.resize(dL);}
+
+
+  void SetReactionLayer(int fL){fReactionLayer = fL;}
+  
+  // getters
+  Int_t GetEventID(){return feventID;}
+  UShort_t GetNDiamonds(){return DimX*DimY*NLayers;}
+  TVector3 GetIncomingDirection(){return fincdir;}
+  TVector3 GetIncomingPosition(){return fincpos;}
+  double GetBeamEnergy(){return fEbeam;}
+
+  TVector3 GetOutgoingDirection(){return foutdir;}
+  TVector3 GetOutgoingPosition(){return foutpos;}
+  vector<double> GetOutGoingBeta(){return fbetas;}
+  double GetOutGoingBeta(int n){return fbetas[n];}
+  
+  TVector3 GetReactionPosition(){return freacpos;}
+  int GetReactionLayer(){return fReactionLayer;}
+
+
+  int GetNLayers(){return NLayers;}
+  int GetDimX(){return DimX;}
+  int GetDimY(){return DimY;}
+
+
+  void Print(Option_t * ="") const override {
+    cout << "event ID = " << feventID << ", NLayers = " << fbetas.size() << endl;
+  } 
+  
+protected:
+  Int_t feventID;
+  TVector3 fincdir;
+  TVector3 fincpos;
+  double fEbeam;
+  TVector3 foutdir;
+  TVector3 foutpos;
+
+  int fReactionLayer;
+
+  vector<double> fbetas;
+  
+  TVector3 freacpos;
+  int DimX ;
+  int DimY ;
+  int NLayers ;
+
+
+  //vector<double> freacpos;
+  
+  ClassDef(SimEvent, 1);
+};
+
+#ifdef ISSIM
+
+class EventInfo : public G4VUserEventInformation {
+public:
+
+  EventInfo(){
+    fsimevent = new SimEvent;
+    flisaevent = new LISAEvent;
+  }
+  SimEvent* GetSimEvent(){return fsimevent;}
+  LISAEvent* GetLISAEvent(){return flisaevent;}
+  
+  void SetSimEvent(SimEvent* simev){fsimevent = simev;}
+  void SetLISAEvent(LISAEvent* lisaev){flisaevent = lisaev;}
+  
+  ~EventInfo(){};
+
+  inline virtual void Print()const{;}
+private:
+  SimEvent* fsimevent;
+  LISAEvent* flisaevent;
+};
+#endif
+
+#endif
